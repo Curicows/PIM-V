54fa26f9b7c42ec1597dc7064b30b2a88c6b3a0e
Giovane Marcelo <giovane0845@gmail.com>
2022-04-16 06:51:48 -0300

    PV-4 - list and remove equipamentos

---
+++ PIM-V/Views/Equipamentos/ListagemEquipamentos.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel;
+
+namespace PIM_V.Views.Equipamentos
+{
+    partial class ListagemEquipamentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LISTAGEM DE EQUIPAMENTOS";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(399, 193);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(10, 72);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Adicionar";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(159, 72);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remover";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(312, 72);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "Editar";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // ListagemEquipamentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 295);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(415, 334);
+            this.MinimumSize = new System.Drawing.Size(415, 334);
+            this.Name = "ListagemEquipamentos";
+            this.Text = "Listagem de equipamentos";
+            this.Load += new System.EventHandler(this.ListagemEquipamentos_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button editButton;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
---
+++ PIM-V/Views/Equipamentos/ListagemEquipamentos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using PIM_V.Classes;
+using PIM_V.Models;
+using System.Linq;
+
+namespace PIM_V.Views.Equipamentos
+{
+    public partial class ListagemEquipamentos : Form
+    {
+        private Collection _collection;
+        public ListagemEquipamentos()
+        {
+            InitializeComponent();
+        }
+
+        private void ListagemEquipamentos_Load(object sender, EventArgs e)
+        {
+            this.FillList();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int[] selectedRows = new int[this.dataGridView1.SelectedCells.Count];
+            int i = 0;
+            foreach (DataGridViewCell selectedCell in this.dataGridView1.SelectedCells)
+            {
+                if (selectedRows.Contains(selectedCell.RowIndex))
+                {
+                    continue;
+                }
+                selectedRows[i] = selectedCell.RowIndex;
+                i++;
+            }
+
+            foreach (int rowId in selectedRows)
+            {
+                if (rowId != 0) {
+                    DataRow row = this._collection.GetDataTable().Rows[rowId];
+                    Equipamento equipamento = new Equipamento();
+                    equipamento.Find((long)row[0]);
+                    equipamento.Delete();
+                }
+            }
+
+            this.FillList();
+        }
+
+        private void FillList()
+        {
+            Equipamento equipamento = new Equipamento();
+            Collection collection = equipamento.GetAll();
+            this.SetCollection(collection);
+            this.dataGridView1.DataSource = collection.GetDataTable();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public void SetCollection(Collection collection)
+        {
+            this._collection = collection;
+        }
+
+        public Collection GetCollection()
+        {
+            return this._collection;
+        }
+    }
+}
