5719d0f9cfaeb47fd4214d043766eff9ee5df959
Giovane Marcelo <giovane0845@gmail.com>
2022-04-18 18:30:35 -0300

    fix invalid migration

--- PIM-V/Migrations/Migration004.cs
+++ PIM-V/Migrations/Migration004.cs
@@ -8,7 +8,7 @@ namespace PIM_V.Migrations
 
         public Migration004()
         {
-            this._sql = "INSERT INTO usuarios (nome, login, senha, email) VALUES ('Administrador', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com');";
+            this._sql = "INSERT INTO usuarios (nome, login, senha, email) VALUES ('Administrador', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com')";
         }
         
         public string GetMigration()
